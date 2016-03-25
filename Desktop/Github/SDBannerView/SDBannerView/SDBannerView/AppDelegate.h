ed6a1c2eaedfda81bcb69fe2468b633d8aed91b9
shendong <shendong@zhouleis-Mac-mini.local>
2016-03-25 14:39:42 +0800

    add helper

---
+++ Desktop/Github/SDBannerView/SDBannerView/SDBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDBannerView
+//
+//  Created by shendong on 16/3/17.
+//  Copyright © 2016年 shendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/Github/SDBannerView/SDBannerView/SDBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDBannerView
+//
+//  Created by shendong on 16/3/17.
+//  Copyright © 2016年 shendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
