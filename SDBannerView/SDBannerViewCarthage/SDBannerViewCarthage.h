8fccf6de8f6263307ebcf9955cbefb5ec268bd32
momo13014 <shendong13014@gmail.com>
2017-03-06 23:51:23 +0800

    add tag

---
+++ SDBannerView/SDBannerViewCarthage/SDBannerViewCarthage.h
@@ -0,0 +1,19 @@
+//
+//  SDBannerViewCarthage.h
+//  SDBannerViewCarthage
+//
+//  Created by Shendong on 2017/3/6.
+//  Copyright © 2017年 shendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDBannerViewCarthage.
+FOUNDATION_EXPORT double SDBannerViewCarthageVersionNumber;
+
+//! Project version string for SDBannerViewCarthage.
+FOUNDATION_EXPORT const unsigned char SDBannerViewCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDBannerViewCarthage/PublicHeader.h>
+
+
